d670a87e07c24a7ca6e29eb6e73cc051d55b431e
Dylan Chan <chan0566@algonquinlive.com>
2024-11-12 18:31:11 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Layout Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Doto'),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Orbitron'),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Basic Layout'),
@@ -19,49 +19,76 @@ class MainApp extends StatelessWidget {
         body: ListView(
           children: [
             SizedBox(
-              height: 400,
-              child: HorizontalScroller(),
+              height: 200,
+              child: topHorizontalScroller(),
             ),
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 20),
             const ListTile(
               leading: Icon(Icons.star),
-              title: Text('ListTile with Regular Font',
-                  style: TextStyle(fontFamily: 'Doto')),
+              title: Text('Satisfy - regular',
+                  style: TextStyle(fontFamily: 'Satisfy')),
             ),
+            const SizedBox(height: 10),
             const ListTile(
-              leading: CircleAvatar(child: Icon(Icons.person)),
-              title: Text('ListTile with Bold Font',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
+              leading: Icon(Icons.star),
+              title: Text('Caveat - regular',
+                  style: TextStyle(fontFamily: 'Caveat')),
             ),
+            const SizedBox(height: 10),
             const ListTile(
               leading: Icon(Icons.settings),
-              title: Text('ListTile with Italic Font',
-                  style: TextStyle(
-                      fontFamily: 'Doto', fontStyle: FontStyle.normal)),
+              title: Text('Orbitron - regular',
+                  style: TextStyle(fontFamily: 'Orbitron')),
             ),
+            const SizedBox(height: 10),
             const ListTile(
-              trailing: Icon(Icons.arrow_forward),
-              title: Text('Default Device Font'),
+              leading: CircleAvatar(child: Icon(Icons.person)),
+              title: Text('Orbitron - bold',
+                  style: TextStyle(
+                      fontFamily: 'Orbitron', fontWeight: FontWeight.bold)),
             ),
             const SizedBox(height: 20), // Space between elements
+            SizedBox(
+              height: 300,
+              child: bottomHorizontalScroller(),
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
     );
   }
 
-  ListView HorizontalScroller() {
+  ListView topHorizontalScroller() {
     return ListView.builder(
       itemCount: 3,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
+        final imageUrls = [
+          'https://plus.unsplash.com/premium_photo-1707353401897-da9ba223f807?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          'https://plus.unsplash.com/premium_photo-1707353402003-effbc48c547d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          'https://plus.unsplash.com/premium_photo-1707353400249-1d96e1a7e0e6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ];
         return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Image.network('https://placehold.co/600x400@2x.png',
-              width: 200, height: 200),
-        );
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Image.network(imageUrls[index], width: 300, height: 200));
+      },
+    );
+  }
+
+  ListView bottomHorizontalScroller() {
+    return ListView.builder(
+      itemCount: 3,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        final images = [
+          'assets/images/Blue-Dog.jpg',
+          'assets/images/Yellow-Dog.jpg',
+          'assets/images/Red-Dog.jpg',
+        ];
+        return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Image.asset(images[index], width: 200, height: 300));
       },
     );
   }
